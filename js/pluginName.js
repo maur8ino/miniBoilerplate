4484ada49f15acd3f116c0a7158f23385056c936
Mauro Verrocchio <mauroverrocchio@gmail.com>
2013-01-06 19:53:57 +0100

    Added a slightly modified version of jQuery plugin boilerplate (from https://github.com/jquery-boilerplate/boilerplate) and added some other test

--- js/pluginName.js
+++ js/pluginName.js
@@ -1,42 +1,78 @@
+/*
+ *  Project:
+ *  Description:
+ *  Author:
+ *  License:
+ */
 
-jQuery(function() {
-  $.pluginName = function(element, options) {
-    var state;
-    state = '';
-    this.settings = {};
-    this.$element = $(element);
-    this.setState = function(_state) {
-      return (state = _state);
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ($, window, document, undefined) {
+
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variable rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = 'pluginName';
+    var defaults = {
+        message: 'messages'
     };
-    this.getState = function() {
-      return state;
+
+    // The actual plugin constructor
+    function Plugin(element, options) {
+        this.$element = $(element);
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        init: function () {
+            // Place initialization logic here
+            // You already have access to the DOM element and
+            // the options via the instance, e.g. this.$element
+            // and this.options
+            // you can add more functions like the one below and
+            // call them like so: this.yourOtherFunction(this.$element, this.options).
+            this.state = 'ready';
+        },
+        yourOtherFunction: function () {
+            // some logic
+        },
+        getState: function() {
+            return this.state;
+        },
+        setState: function(state) {
+            this.state = state;
+        },
+        somePublicMethod: function() {
+          return somePrivateMethod();
+        }
     };
-    this.getSetting = function(key) {
-      return this.settings[key];
+
+    function somePrivateMethod() {
+      return 'this is a private method';
+    }
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            }
+        });
     };
-    this.callSettingFunction = function(name, args) {
-      if (args === null) {
-        args = [];
-      }
-      return this.settings[name].apply(this, args);
-    };
-    this.init = function() {
-      this.settings = $.extend({}, this.defaults, options);
-      return this.setState('ready');
-    };
-    this.init();
-    return this;
-  };
-  $.pluginName.prototype.defaults = {
-    message: 'Hello world'
-  };
-  return $.fn.pluginName = function(options) {
-    return this.each(function() {
-      var plugin;
-      if ($(this).data('pluginName') === void 0) {
-        plugin = new $.pluginName(this, options);
-        return $(this).data('pluginName', plugin);
-      }
-    });
-  };
-});
+
+})(jQuery, window, document);
---
+++ spec/javascripts/pluginNameSpec.js
@@ -0,0 +1,53 @@
+
+describe('PluginName', function() {
+  var options;
+  options = {
+    message: 'Hello World'
+  };
+  beforeEach(function() {
+    loadFixtures('fragment.html');
+    return this.$element = $('#fixtures');
+  });
+  describe('plugin behavior', function() {
+    it('should be available on the jQuery object', function() {
+      return expect($.fn.pluginName).toBeDefined();
+    });
+    it('should be chainable', function() {
+      return expect(this.$element.pluginName()).toBe(this.$element);
+    });
+    it('should offers default values', function() {
+      var plugin;
+      this.$element.pluginName();
+      plugin = this.$element.data('plugin_pluginName');
+      return expect(plugin._defaults).toBeDefined();
+    });
+    it('should overwrites the settings', function() {
+      var plugin;
+      this.$element.pluginName(options);
+      plugin = this.$element.data('plugin_pluginName') ;
+      return expect(plugin.options.message).toBe(options.message);
+    });
+  });
+  describe('plugin functionality', function() {
+    beforeEach(function() {
+      this.$element.pluginName();
+      return this.plugin = this.$element.data('plugin_pluginName');
+    });
+    it('should have a ready state', function() {
+      return expect(this.plugin.getState()).toBe('ready');
+    });
+    it('should be updatable', function() {
+      this.plugin.setState('new state');
+      return expect(this.plugin.getState()).toBe('new state');
+    });
+    it('should expose public methods', function() {
+      return expect(this.plugin.somePublicMethod).toBeDefined();      
+    });
+    it('should not expose private methods', function() {
+      return expect(this.plugin.somePrivateMethod).toBeUndefined();      
+    });
+    it('should call a private method inside a public methods', function() {
+      return expect(this.plugin.somePublicMethod()).toBe('this is a private method');      
+    });
+  });
+});
